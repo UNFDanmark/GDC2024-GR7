69930ba696c7ad2bd16104e32d44cceed8a151a8
Deltager <teknisk+GDC2024-GR7@game.unf.dk>
2024-07-15 12:26:48 +0200

    Starting SUMO

--- TheUnityProject/Assets/KnockbackBumper.cs
+++ TheUnityProject/Assets/KnockbackBumper.cs
@@ -7,7 +7,17 @@ public class KnockbackBumper : MonoBehaviour
 {
     public float knockbackValue;
 
-    public Rigidbody rb;
+    public Rigidbody rbPlayer;
+    public Rigidbody rbEnemy;
+    public Transform TRplayer;
+    public Transform TRenemy;
+    public Transform knockbackOrigin;
+    //public Transform Knockback;
+    public float explosionMultiplier = 1000f;
+    public float speedKnockback;
+    public float explosionRadius = 20f;
+    public string enemytag;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +28,29 @@ public class KnockbackBumper : MonoBehaviour
     void Update()
     {
         
+        
     }
 
-    void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.tag == "car")
-        {
-            Vector3 direction = (transform.position - other.transform.position).normalized;
-            rb.AddForce(direction * knockbackValue);
-        }
-    }
+   // void OnTriggerEnter(Collider other)
+    //{ 
+        
+       // Debug.Log("Triggered");
+        
+       // if (other.gameObject.CompareTag(enemytag))
+       // {
+           // Vector3 positionV3 = knockbackOrigin.position;
+            //Vector3 ResultingKnockback = TRplayer.forward * speedKnockback + TRenemy.forward;
+            //rbEnemy.AddForce(ResultingKnockback);
+            //rbPlayer2.AddExplosionForce(explosionMultiplier, positionV3,explosionRadius, 100f);
+           // Vector3 vel = rbPlayer2.velocity;
+            //vel.y = 100f;
+            //bPlayer2.velocity = vel;
+            //Debug.Log("POW!");
+            //Vector3 direction = (transform.position - other.transform.position).normalized;
+            //rbPlayer1.AddForce(direction * knockbackValue);
+            
+            
+        //}
+    //}
 }
 
